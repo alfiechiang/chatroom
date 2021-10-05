36e6c626fd70a1b56b89ae4d22da273677da4d14
alfiechiang <i44711015@gmail.com>
2021-10-05 23:21:04 +0800

    update

--- backend/main.go
+++ backend/main.go
@@ -2,28 +2,31 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
-	"net/http"
+
+	// "net/http"
 	"time"
 
+	"github.com/gin-gonic/gin"
 	"nhooyr.io/websocket"
 	"nhooyr.io/websocket/wsjson"
 )
 
 func main() {
 
-	http.HandleFunc("/ws", func(rw http.ResponseWriter, r *http.Request) {
-
-		conn, err := websocket.Accept(rw, r,  &websocket.AcceptOptions{InsecureSkipVerify: true})
+	r := gin.Default()
+	r.GET("/ws", func(c *gin.Context) {
+		conn, err := websocket.Accept(c.Writer, c.Request, &websocket.AcceptOptions{InsecureSkipVerify: true})
 		if err != nil {
 			log.Println(err)
 			return
 		}
-
 		defer conn.Close(websocket.StatusInternalError, "Internal Error")
-		ctx, cancel := context.WithTimeout(r.Context(), time.Second*30)
-		defer cancel()
 
+		ctx, cancel := context.WithTimeout(c, time.Second*30)
+		defer cancel()
+		fmt.Println("Hello World")
 		var v interface{}
 		err = wsjson.Read(ctx, conn, &v)
 		if err != nil {
@@ -31,8 +34,10 @@ func main() {
 			return
 		}
 		conn.Close(websocket.StatusNormalClosure, "")
-
 	})
-	log.Fatal(http.ListenAndServe(":8000", nil))
+
+	r.Run(":2022")
+
+	
 
 }
